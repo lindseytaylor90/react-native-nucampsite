6e9c3bbb7cd89e7cd5c3c9109ca86af986afcb61
Lindseytaylor90 <tylindsey90@gmail.com>
2021-03-21 11:23:15 -0600

    share campsite

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon  } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 
-function RenderContact() {
+function RenderContact(props) {
     return (
         <ScrollView>
             <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
@@ -23,6 +24,17 @@ function RenderContact() {
                 <Text>
                     Email: campsites@nucamp.co 
                 </Text>
+                <Button
+                    title="Send Email"
+                    buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                    icon={<Icon
+                        name='envelope-o'
+                        type='font-awesome'
+                        color='#fff'
+                        iconStyle={{marginRight: 10}}
+                    />}
+                    onPress={() => props.sendMail()}
+                />
             </Card>
             </Animatable.View>
         </ScrollView>
@@ -36,10 +48,18 @@ class Contact extends Component {
         title: 'Contact Us'
     }
 
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        });
+    }
+
     render(){
         return(
             <ScrollView>
-                <RenderContact/>
+                <RenderContact sendMail={this.sendMail}/>
             </ScrollView>
         );
     }
